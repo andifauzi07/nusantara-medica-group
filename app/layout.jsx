f88e6347ef945f96e2dde88eb912364358d8a453
andi fauzi <andifauzi0709@gmail.com>
2025-06-29 17:23:20 +0800

    feat: add metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,8 +10,28 @@ const montserrat = Montserrat({
 });
 
 export const metadata = {
-	title: 'Nusantara Medika Group',
-	description: 'Solusi Herbal Terpercaya Untuk Menjaga Keseimbangan Tubuh Tetap Terjaga',
+	title: 'Nusantara Medika Herbal',
+	description: 'Nusantara Medika Herbal hadir sebagai solusi herbal terpercaya untuk menjaga keseimbangan dan kesehatan tubuh secara alami.',
+	openGraph: {
+		title: 'Nusantara Medika Herbal',
+		description: 'Kenali lebih dekat Nusantara Medika Herbal dan visi kami dalam menghadirkan solusi kesehatan alami berbasis herbal terpercaya di Indonesia.',
+		url: 'https://nmamedikaherbal.store/',
+		siteName: 'Nusantara Medika Herbal',
+		type: 'website',
+		images: [
+			{
+				url: 'https://nmamedikaherbal.store/dekstop/detail-dekstop.jpeg',
+				width: 1200,
+				height: 630,
+				alt: 'Tim Nusantara Medika Herbal',
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Nusantara Medika Herbal',
+		description: 'Pelajari siapa kami, nilai-nilai yang kami pegang, dan misi kami dalam menyediakan produk herbal terpercaya.',
+	},
 };
 
 export default function RootLayout({ children }) {
